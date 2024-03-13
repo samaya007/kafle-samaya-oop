0b8dd4296cb531f477514330eda9b76ad9cba77d
Samaya Kafle <worksamaya@gmail.com>
2024-03-13 12:48:28 -0400

    Updated basic js and added comments

--- js/modules/GameManager.js
+++ js/modules/GameManager.js
@@ -7,9 +7,9 @@ export class GameManager {
     this.messageElement = messageElement;
     this.gameBoard = new GameBoard(gameBoardElement, this);
     this.triesLeft = 20;
-    this.updateGameState(false);
+    this.updateGameState(false); //initial value false meaning no tile in winning row/coloumn
   }
-
+//handles win and loss 
   updateGameState(tileInWinningLine) {
     this.triesLeftElement.textContent = `Tries Left: ${this.triesLeft}`;
     if (tileInWinningLine && this.gameBoard.checkForWin()) {
@@ -20,8 +20,11 @@ export class GameManager {
   }
 
   handleTileClick(tile) {
+    //DO  NOT TOUCH took a while to fix simple typo error:
+    //this checks if the clicked dtile has not been revealed and if at the same time if ther are remainig tries
+    //and reveals the cliked tile on board
     if (!tile.classList.contains("revealed") && this.triesLeft > 0) {
-      this.gameBoard.revealTile(tile);
+      this.gameBoard.revealTile(tile); 
       if (!this.gameBoard.isTileInWinningLine(parseInt(tile.dataset.index))) {
         this.triesLeft--;
       }
@@ -30,22 +33,23 @@ export class GameManager {
       );
     }
   }
-
+//winners message
   handleWin() {
     this.messageElement.textContent = "Congratulations, you've won the jackpot ! Restart to play again";
     this.messageElement.classList.remove("hidden");
     this.messageElement.classList.add("win-message");
   }
-
+//,oosing message
   handleLose() {
     this.messageElement.textContent =
       "Better luck next time. Please restart the game.";
     this.messageElement.classList.remove("hidden");
     this.messageElement.classList.add("lose-message");
   }
-
+// to reset the game
   resetGame() {
     this.gameBoard.initializeBoard();
+    //reset tge number of tris 
     this.triesLeft = 20;
     this.updateGameState(false);
     this.messageElement.textContent = ""; // clears message
